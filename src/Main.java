0dc97e94c0a4554edc7b84ec6b3f8e48c7c64928
DurgeshKavate003 <durgesh.kavate@outlook.com>
2023-03-29 16:40:48 +0530

    code V2 - the gameloop

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ package src;
 public class Main {
     public static void main(String[] args) {
         Window window = new Window();
-        Thread t1 = new Thread();
+        Thread t1 = new Thread(window);
 
         t1.start();
     }
--- src/Window.java
+++ src/Window.java
@@ -1,9 +1,13 @@
 package src;
 
 import javax.swing.JFrame;
+import java.awt.Graphics2D;
+import java.awt.Color;
 
 
 public class Window extends JFrame implements Runnable {
+
+    Graphics2D g2;
 
     public Window() {
         this.setSize(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
@@ -11,11 +15,29 @@ public class Window extends JFrame implements Runnable {
         this.setResizable(false);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        g2 = (Graphics2D)this.getGraphics();
+    }
+
+    public void update(double dt) {
+        g2.setColor(Color.BLACK);
+        g2.fillRect(0, 0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
     }
 
     public void run() {
+        double lastFrameTime = 0.0;
         while (true) {
+            double time = Time.getTime();
+            double deltaTime = time - lastFrameTime;
+            lastFrameTime = time;
 
+            update(deltaTime);
+
+            try {
+                Thread.sleep(30);
+            } catch (Exception e){
+
+            }
         }
     }
 }
